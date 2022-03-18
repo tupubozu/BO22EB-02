f1b52edda8d4d0347a0488d444e55300dd886cfe
Kay Siver Bø <578167@stud.hvl.no>
2022-03-18 13:45:35 +0100

    Default argument value in GetAes-method in crypto library

--- Automation.Cryptography/Crypto.cs
+++ Automation.Cryptography/Crypto.cs
@@ -5,14 +5,15 @@ namespace Automation.Cryptography
 {
     public static class Crypto
     {
-        public static Aes GetAes(byte[] iv)
+        public static Aes GetAes(byte[] iv = null)
         {
             var aesCrypto = Aes.Create();
             aesCrypto.BlockSize = 128;
             aesCrypto.KeySize = 256;
             aesCrypto.Mode = CipherMode.CBC;
             aesCrypto.Padding = PaddingMode.ISO10126;
-            aesCrypto.IV = iv;
+            if (!(iv is null))
+                aesCrypto.IV = iv;
             return aesCrypto;
         }
     }
