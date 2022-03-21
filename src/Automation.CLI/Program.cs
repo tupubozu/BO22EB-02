2a64b6a3162c7d5e82bb2272c73a47a19c78a19d
Kay Siver Bø <578167@stud.hvl.no>
2022-03-21 17:14:36 +0100

    Convert GUI and CLI projects to .NET 6 to enable self-contained deployment, and keeping C# 7.3 as standard language to support regression if needed

---
+++ src/Automation.CLI/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using Automation.Configuration;
+using Automation.Cryptography;
+using Renci.SshNet;
+using Serilog;
+using System.IO;
+using System.IO.Compression;
+using System.Security.Cryptography;
+
+namespace Automation.CLI
+{
+    internal static class Program
+    {
+        static ProgramConfiguration config;
+        static void Main(string[] args)
+        {
+            InitializeLog();
+            Log.Information("Starting program");
+
+            if (args.Length == 0)
+            {
+                Log.Fatal("No arguments provided");
+                Log.Information("Exiting");
+#if DEBUG
+                Console.ReadKey(true);
+#endif
+                return;
+            }
+
+            Log.Information("Configuration argument: \"{0}\"", args[0]);
+            if (!File.Exists(args[0]))
+            {
+                Log.Fatal("No file exist at \"{0}\"", args[0]);
+                Log.Information("Exiting");
+                return;
+            }
+            string configArchivePath = Path.GetFullPath(args[0]);
+            Log.Information("Reading configuration from: \"{0}\"", configArchivePath);
+
+            try
+            {
+                using (var configArchiveStream = File.Open(configArchivePath, FileMode.Open, FileAccess.Read))
+                using (var zipConfig = new ZipArchive(configArchiveStream, ZipArchiveMode.Read, true, Encoding.UTF8))
+                using (var aesCrypto = Crypto.GetAes(Encoding.UTF8.GetBytes("I'm an engineer!")))
+                using (var passwordHash = SHA256.Create())
+                {
+                    Console.Write("Password: ");
+                    aesCrypto.Key = passwordHash.ComputeHash(new MemoryStream(Encoding.UTF8.GetBytes(Console.ReadLine())));
+
+                    var encryptedConfig = zipConfig.GetEntry("config.xml.aes");
+
+                    using (CryptoStream cryptoConfigStream = new CryptoStream(encryptedConfig.Open(), aesCrypto.CreateDecryptor(), CryptoStreamMode.Read))
+                    {
+                        config = ProgramConfiguration.Parse(cryptoConfigStream);
+                    }
+                }
+
+                Log.Information("Read configuration \"{0}\" made by {1} ({2}) on {3:O}", config.Metadata.Title, config.Metadata.Author.Name, config.Metadata.Author.Email,config.Metadata.Revision);
+                Console.WriteLine("Description: {0}",config.Metadata.Description);
+            }
+            catch (Exception ex)
+            {
+                Log.Debug("Exception: {0}", ex);
+                Log.Error("Exception: {0}", ex.Message);
+                Log.Fatal("Error encountered while reading configuration: {0}: {1}", ex.GetType().FullName, ex.Message);
+                return;
+            }
+
+            Log.Information("Exiting");
+#if DEBUG
+            Console.ReadKey(true);
+#endif
+        }
+        
+        static void InitializeLog()
+        {
+            var logConfig = new LoggerConfiguration()
+#if DEBUG
+                .WriteTo.Console(Serilog.Events.LogEventLevel.Verbose)
+#else
+				.WriteTo.Console(Serilog.Events.LogEventLevel.Information)
+				.WriteTo.Console(Serilog.Events.LogEventLevel.Warning)
+				.WriteTo.Console(Serilog.Events.LogEventLevel.Error)
+#endif
+                .Enrich.FromLogContext()
+                .MinimumLevel.Debug();
+
+            Log.Logger = logConfig.CreateLogger();
+        }
+    }
+}
---
+++ src/Automation.ConfigMaker.GUI/ScriptPropertiesForm.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace Automation.ConfigMaker.GUI
+{
+    partial class ScriptPropertiesForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.fileBrowseButton = new System.Windows.Forms.Button();
+            this.scriptSourceTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.scriptNameTextBox = new System.Windows.Forms.TextBox();
+            this.okButton = new System.Windows.Forms.Button();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.groupBox1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.groupBox2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.groupBox3, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.okButton, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.cancelButton, 1, 2);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 34F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(484, 461);
+            this.tableLayoutPanel1.TabIndex = 5;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Location = new System.Drawing.Point(3, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(236, 207);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Script type";
+            this.groupBox1.Visible = false;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(23, 30);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(54, 17);
+            this.radioButton1.TabIndex = 1;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "BASH";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(23, 53);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(76, 17);
+            this.radioButton2.TabIndex = 2;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Powershell";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.groupBox2.Controls.Add(this.fileBrowseButton);
+            this.groupBox2.Controls.Add(this.scriptSourceTextBox);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(3, 216);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(236, 207);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "File";
+            // 
+            // fileBrowseButton
+            // 
+            this.fileBrowseButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.fileBrowseButton.Location = new System.Drawing.Point(155, 17);
+            this.fileBrowseButton.Name = "fileBrowseButton";
+            this.fileBrowseButton.Size = new System.Drawing.Size(75, 23);
+            this.fileBrowseButton.TabIndex = 2;
+            this.fileBrowseButton.Text = "Browse";
+            this.fileBrowseButton.UseVisualStyleBackColor = true;
+            this.fileBrowseButton.Click += new System.EventHandler(this.fileBrowseButton_Click);
+            // 
+            // scriptSourceTextBox
+            // 
+            this.scriptSourceTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.scriptSourceTextBox.Enabled = false;
+            this.scriptSourceTextBox.Location = new System.Drawing.Point(65, 19);
+            this.scriptSourceTextBox.Name = "scriptSourceTextBox";
+            this.scriptSourceTextBox.Size = new System.Drawing.Size(84, 20);
+            this.scriptSourceTextBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "File path: ";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox3.Controls.Add(this.label2);
+            this.groupBox3.Controls.Add(this.scriptNameTextBox);
+            this.groupBox3.Location = new System.Drawing.Point(245, 3);
+            this.groupBox3.Name = "groupBox3";
+            this.tableLayoutPanel1.SetRowSpan(this.groupBox3, 2);
+            this.groupBox3.Size = new System.Drawing.Size(236, 420);
+            this.groupBox3.TabIndex = 6;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Attributes";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Name: ";
+            // 
+            // scriptNameTextBox
+            // 
+            this.scriptNameTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.scriptNameTextBox.Location = new System.Drawing.Point(53, 19);
+            this.scriptNameTextBox.Name = "scriptNameTextBox";
+            this.scriptNameTextBox.Size = new System.Drawing.Size(174, 20);
+            this.scriptNameTextBox.TabIndex = 0;
+            this.scriptNameTextBox.Leave += new System.EventHandler(this.scriptNameTextBox_Leave);
+            // 
+            // okButton
+            // 
+            this.okButton.Dock = System.Windows.Forms.DockStyle.Right;
+            this.okButton.Location = new System.Drawing.Point(164, 429);
+            this.okButton.Name = "okButton";
+            this.okButton.Size = new System.Drawing.Size(75, 29);
+            this.okButton.TabIndex = 7;
+            this.okButton.Text = "OK";
+            this.okButton.UseVisualStyleBackColor = true;
+            this.okButton.Click += new System.EventHandler(this.okButton_Click);
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.cancelButton.Dock = System.Windows.Forms.DockStyle.Left;
+            this.cancelButton.Location = new System.Drawing.Point(245, 429);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(75, 29);
+            this.cancelButton.TabIndex = 8;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
+            // 
+            // ScriptPropertiesForm
+            // 
+            this.AcceptButton = this.okButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.cancelButton;
+            this.ClientSize = new System.Drawing.Size(484, 461);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.MinimumSize = new System.Drawing.Size(350, 250);
+            this.Name = "ScriptPropertiesForm";
+            this.Text = "Script Properties";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button fileBrowseButton;
+        private System.Windows.Forms.TextBox scriptSourceTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox scriptNameTextBox;
+        private System.Windows.Forms.Button okButton;
+        private System.Windows.Forms.Button cancelButton;
+    }
+}
